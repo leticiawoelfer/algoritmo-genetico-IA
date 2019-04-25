95fed2026346527617e01e1134b6448069d76084
Douglas Portugal de Oliveira <douglas-portuga@hotmail.com>
2019-04-24 21:56:51 -0300

    Geração de numeros aleatorios

---
+++ algoritmo-genetico.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 20:57:25 2019
+
+@author: _-Douglas-_
+"""
+import random
+
+
+
+for i in range(1, 20):
+    linha2 = i
+    i+=1
+    x = round(random.uniform(0.0, 1.0),2)
+    y = round(random.uniform(0.0, 1.0),2)
+    print(x)
+    #print(y)
+    
+print ("")
+   
+for i in range(1, 20):
+    linha2 = i
+    i+=1
+    x = round(random.uniform(0.0, 1.0),2)
+    y = round(random.uniform(0.0, 1.0),2)
+    #print(x)
+    print(y)
