1d66e11ffe3d9a936ca96647cbdac3388fc63b15
leticiawoelfer <leticia.woelfer@hotmail.com>
2019-04-28 00:59:01 -0300

    no message

--- algoritmo-genetico.py
+++ algoritmo-genetico.py
@@ -2,60 +2,59 @@
 """
 Created on Wed Apr 24 20:57:25 2019
 
-@author: _-Douglas-_
+@author: Douglas, Joana e Leticia
 """
 import random
 
-#gerar uma matriz 20x20
-
-
 def criaMatrizCromossomos():
-    for x in range(1, 21):
+    #gerar uma matriz 20x20 de numero inteiro de 1 a 20
+    for x in range(0, 19):
         linha = random.sample(range(1,21),20);
         matriz.append(linha);
     #print(matriz);
     
 def criarMatrizValores():
-    x = [];
-    y = [];
-    line = [];
-    #monta as duas listas 
-    for i in range(1, 21):
-        x.append(round(random.uniform(0.0, 1.0),4));
-        y.append(round(random.uniform(0.0, 1.0),4));
-    #montar a linha de valores para adicionar na matriz de valores
-    for linha in range(0, 22):
-        for coluna in range(0, 22):
-            line.append(x[linha] - y[coluna]);
-        matrizValores.append(line);
+    x = []
+    y = []
+    #monta as duas listas de numero aleatorios entre 0.0 e 1.0
+    for i in range(0, 20):
+        x.append(round(random.uniform(0.0, 1.0),4))
+        y.append(round(random.uniform(0.0, 1.0),4))
     
-    print(x);
-    print(y);
-    print(matrizValores);
-        #for j in range()
+    #gera a matriz numerica de 20x20 
+    for linha in range(0, 20):
+        line = []#limpa a var line sempre que for pra próxima linha
+        for coluna in range(0, 20):
+            valor = 0
+            #print("linha ={0}".format(linha))
+            #print("coluna ={0}".format(coluna))
+            #print(type(linha));
+            #print(type(coluna));
+            if linha != coluna:
+                #print("x[{0}] - y[{1}]".format(linha, coluna))
+                valor = round(x[linha] - y[coluna], 4)
+                #print("{0} - {1} = {2}".format(x[linha], y[coluna], valor))
+            #print("adiciona valor na line ={0}".format(valor))
+            line.append(valor)            
+        #print("adiciona valor na matrizValores ={0}".format(line))
+        matrizValores.append(line)
+    #print(matrizValores)
     
-
-
-"""for i in range(1, 20):
-    linha2 = i
-    i+=1
-    x = round(random.uniform(0.0, 1.0),2)
-    y = round(random.uniform(0.0, 1.0),2)
-    print(x)
-    #print(y)
+def criarListaDistancias():
+    for linha in matrizValores:
+        print(linha[1])
     
-print ("")
-   
-for i in range(1, 20):
-    linha2 = i
-    i+=1
-    x = round(random.uniform(0.0, 1.0),2)
-    y = round(random.uniform(0.0, 1.0),2)
-    #print(x)
-    print(y)
-""" 
+    
+    
 matriz = [];
 matrizValores = [];
+listaDistancias = [];
 
+'''gerar matriz 20x20 de numeros inteiros'''
 criaMatrizCromossomos();
+'''gerar matriz valorada de 20x21'''
 criarMatrizValores();
+'''gerar a matriz com o calculo sa distancias'''
+criarListaDistancias();
+
+
