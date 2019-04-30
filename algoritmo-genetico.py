27984b11e9825ce8cb12b727758f0c3dd078f642
Douglas Portugal de Oliveira <douglas-portuga@hotmail.com>
2019-04-29 21:57:12 -0300

    Metodo de ordenação apenas dos indices, para gerar a nova matriz ja ordenada

--- algoritmo-genetico.py
+++ algoritmo-genetico.py
@@ -6,6 +6,7 @@ Created on Wed Apr 24 20:57:25 2019
 """
 import random
 import matplotlib.pyplot as plt
+import numpy
 
 def criaMatrizPopulacao():
     #gerar uma matriz 20x20 de numero inteiro de 1 a 20
@@ -22,7 +23,6 @@ def criarMatrizDistancias():
         x.append(round(random.uniform(0.0, 1.0),4))
         y.append(round(random.uniform(0.0, 1.0),4))
 
-
     #Gera matriz das distancias de cada cidade
     for linha in range(0, 20):
         line = []#limpa a var line sempre que for pra próxima linha
@@ -31,42 +31,42 @@ def criarMatrizDistancias():
             line.append(valor)
         matrizDistancias.append(line)
 
-
-
 def criarListaDistancias():
-
+    #Cria a lista com a soma das distancias das cidades
     for linha in range(0, 20):
-        total=0
+        total=0#Limpa valor total a cada troca de individuo
         for coluna in range(0, 20):
-            if coluna == 19:
+            if coluna == 19:#Condição para finalizar na cidade que iniciou
                 total=total+(matrizDistancias[matrizPopulacao[linha][coluna]-1][matrizPopulacao[linha][0]-1])             
             else:
                 total=total+(matrizDistancias[matrizPopulacao[linha][coluna]-1][matrizPopulacao[linha][coluna+1]-1])
-            
-
         listaDistancias.append(total)
 
-            
-#            print (str(matrizPopulacao[linha][coluna]-1) + ' ' + str(matrizPopulacao[linha][coluna+1]-1)) 
-            
 
-#        listaDistancias.append(10)
-#            valor = round(((x[linha]-x[coluna])**2+(y[linha]-y[coluna])**2)**(1/2), 4)
-            
-#        dist(i,1)=dist(i)+dcidade(tour(i,j),tour(i,j+1)); 
-
-
-
-
-
-
+def exibeResultados(resultados):
 #Gera grafico com os valores da função aptidão
-plt.xlabel('Indivíduos')
-plt.ylabel('Aptidão')
-plt.title('Aptidão dos indivíduos')
-plt.plot(listaDistancias)        
-plt.show()
-   
+    plt.xlabel('Indivíduos')
+    plt.ylabel('Aptidão')
+    plt.title('Aptidão dos indivíduos')
+    plt.plot(resultados)        
+    plt.show()
+
+def ordenaListadeResultados(desordenado):
+    ordenado = [];
+    ordenado.append(numpy.argsort(desordenado))
+    return ordenado
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 matrizPopulacao = [];
@@ -80,5 +80,8 @@ criaMatrizPopulacao();
 criarMatrizDistancias();
 '''gerar a matriz com o calculo sa distancias'''
 criarListaDistancias();
-
-
+'''Exibe resultado em modo grafio'''
+exibeResultados(listaDistancias);
+'''Ordenar um array retornando apenas o index'''
+teste = ordenaListadeResultados(listaDistancias)
+print(teste)
