d8d0e361c901f643ab16a0a341200163af264721
Douglas Portugal de Oliveira <douglas-portuga@hotmail.com>
2019-05-07 23:24:03 -0300

    Random efetuado, só pendente comecar as reconbinações , matriz estão com 10 individuos

--- algoritmo-genetico.py
+++ algoritmo-genetico.py
@@ -12,7 +12,8 @@ import numpy as np
 matrizPopulacao = []
 matrizDistancias = []
 listaDistancias = []
-distanciasOrdenadas = []
+indicesOrdenados = []
+resultados=[]
 
 
 def criaMatrizPopulacao():
@@ -43,18 +44,18 @@ def criarListaDistancias():
         total=0#Limpa valor total a cada troca de individuo
         for coluna in range(0, len(matrizPopulacao[0])):
             if coluna == 19:#Condição para finalizar na cidade que iniciou
-                total=total+(matrizDistancias[matrizPopulacao[linha][coluna]-1][matrizPopulacao[linha][0]-1])             
+                total=round(total+(matrizDistancias[matrizPopulacao[linha][coluna]-1][matrizPopulacao[linha][0]-1]),4)             
             else:
-                total=total+(matrizDistancias[matrizPopulacao[linha][coluna]-1][matrizPopulacao[linha][coluna+1]-1])
+                total=round(total+(matrizDistancias[matrizPopulacao[linha][coluna]-1][matrizPopulacao[linha][coluna+1]-1]),4)
         listaDistancias.append(total)
-
+        resultados.append(total)
 
 def exibeResultados():
 #Gera grafico com os valores da função aptidão
     plt.xlabel('Indivíduos')
     plt.ylabel('Aptidão')
     plt.title('Aptidão dos indivíduos')
-    plt.plot(listaDistancias)
+    plt.plot(resultados)
     plt.axis([0, 100, 5, 15])
     plt.show()
     
@@ -64,31 +65,66 @@ def exibeResultados():
     print ('Tamanho da população: '+ str(len(matrizPopulacao)))
     
     #Exibe taxa de mutação na legenda
-    print ('Taxa de mutação: '+ str(listaDistancias[distanciasOrdenadas[0][0]]))
+    print ('Taxa de mutação: '+ str(listaDistancias[0]))
 
     #Exibe numero de cidades na legenda
     print ('Numero de cidades: '+ str(len(matrizPopulacao[0])))
     
     #Exibe o melhor custo na legenda
-    print ('Melhor custo: '+ str(listaDistancias[distanciasOrdenadas[0][0]]))
+    print ('Melhor custo: '+ str(listaDistancias[0]))
     
     #Exibe a melhor solução na legenda
-    print ('Melhor solução: '+ str(matrizPopulacao[distanciasOrdenadas[0][0]]))
+    print ('Melhor solução: '+ str(matrizPopulacao[0]))
 
 
 
-def ordenaMatrizDistancias():
+def ordenaMatrizes():
     matrizPopulacaoSort = []
     matrizDistanciasSort = []
-    distanciasOrdenadas.append(np.argsort(listaDistancias))
-
+    temp=[]
+    
+    #Ordenando distancias na lista
+    indicesOrdenados.append(np.argsort(listaDistancias))
+    
+    
+    #Gerando matriz temporaria de distancias
+    for distancias in range(0,  20):
+        temp.append(listaDistancias[indicesOrdenados[0][distancias]])
+    
+    #Atualizando variavel original com a ordenação
+    for d in range(0, 20):
+        listaDistancias[d]=temp[d]    
+    
+    #Organiza as matrizes de população e distancias com base na ordenação das distancias        
     for individuo in range(0,  20):
-        matrizPopulacaoSort.append(matrizPopulacao[distanciasOrdenadas[0][individuo]])
-        matrizDistanciasSort.append(matrizDistancias[distanciasOrdenadas[0][individuo]])
+        matrizPopulacaoSort.append(matrizPopulacao[indicesOrdenados[0][individuo]])
+        
+        #talvez a da distancia possa ser ignorada - Validar
+        matrizDistanciasSort.append(matrizDistancias[indicesOrdenados[0][individuo]])
+        
+    #atualiza matriz antiga com os valores ordenados
+    for i in range(0, 20):
+        matrizPopulacao[i]=matrizPopulacaoSort[i]
+        matrizDistancias[i]=matrizDistanciasSort[i]
+        
 
+def removerMetade():
+    #Remoção dos 10 piores individuos 
+    del matrizPopulacao[9:19]
+    del matrizDistancias[9:19]
+    del listaDistancias[9:19]        
+        
+
+
+
+#    Limpa as matrizes temporarias
+#    matrizDistanciasSort = []
+#    matrizPopulacaoSort =[]
+
+    
 #Não sei como colocar os valores de uma matriz na outra, não assim:        
-#    matrizDistancias = matrizDistanciasSort
-#    matrizPopulacao = matrizPopulacaoSort
+#   matrizDistancias = matrizDistanciasSort
+#   matrizPopulacao = matrizPopulacaoSort
 
     
     
@@ -107,12 +143,21 @@ def ordenaMatrizDistancias():
 
 '''gerar matriz 20x20 de numeros inteiros'''
 criaMatrizPopulacao();
+
 '''gerar matriz valorada de 20x21'''
 criarMatrizDistancias();
+
 '''gerar a matriz com o calculo sa distancias'''
 criarListaDistancias();
+
 '''Ordenar um array retornando apenas o index'''
-ordenaMatrizDistancias()
+ordenaMatrizes()
+
+'''Remover metade da população'''
+removerMetade();
+
 '''Exibe resultado em modo grafio'''
 exibeResultados();
+
+
 
