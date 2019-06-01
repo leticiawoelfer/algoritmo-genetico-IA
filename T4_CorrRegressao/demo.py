bdb1b12a66746f2399f6f2a532f0d91910b4cbfc
leticiawoelfer <leticia.woelfer@hotmail.com>
2019-05-31 21:10:48 -0300

    fase 1 finalizada

--- T4_CorrRegressao/demo.py
+++ T4_CorrRegressao/demo.py
@@ -30,11 +30,10 @@ def correlacao(x, y):
     raizQuad = math.sqrt(somaMediaX * somaMediaY)
     
     r = round((somaR1/raizQuad),4)
-    print("r="+str(r))
     return r;
 
     
-def regressao(x, y):
+def regressao(x, y, r):
     n = len(x)
     somax = 0
     for numX in x:
@@ -54,55 +53,44 @@ def regressao(x, y):
     
     b1 = round((soma1 / soma2),4)
     b0 = round((mediaY - (b1 * mediaX)),4)
-    print("B0="+str(b0))
-    print("B1="+str(b1))
-    linhaRegressao(b0, b1, x)
+    linhaRegressao(b0, b1, x, y, r)
     
-def graficoDispersao(x, y):
-    plt.scatter(x, y)
     
-def linhaRegressao(b0, b1, x):
+def linhaRegressao(b0, b1, x, y, r):
     resultados=[]
-    #resultados.append(0)
     for num in x:
         resultados.append(b0 + (b1 * num))
-    print(resultados)
-    resultados = sorted(resultados)
-    print(resultados)
-    fim = round(((resultados[len(resultados)-1])+1),0)
-    print(fim)
-    resultados.append(fim)
-    print(resultados)
-    plt.plot(resultados)
-    
+    geraGrafico(b0, b1, x, y, r, resultados)
 
+def geraGrafico(b0, b1, x, y, r, resultados):
+    plt.scatter(x, y)
+    plt.plot(x,resultados)
+    plt.title("Correlação:"+str(r)+"  B0:"+str(b0)+"  B1:"+str(b1))
+    plt.figure()
 
 def dataset1():
     x1 = [10,8,13,9,11,14,6,4,12,7,5]
     y1 = [8.04,6.95,7.58,8.81,8.33,9.96,7.24,4.26,10.84,4.82,5.68]
-    graficoDispersao(x1,y1)
-    print("Dados 1")
-    r = correlacao(x1,y1)
-    regressao(x1, y1)
-    
+    print("Dataset 1")
+    r = correlacao(x1, y1)
+    regressao(x1, y1, r)
 
+def dataset2():
+    x2 = [10,8,13,9,11,14,6,4,12,7,5]
+    y2 = [9.14,8.14,8.47,8.77,9.26,8.10,6.13,3.10,9.13,7.26,4.74]
+    print("Dataset 2")
+    r = correlacao(x2, y2)
+    regressao(x2, y2, r)
+
+def dataset3():
+    x3 = [8,8,8,8,8,8,8,8,8,8,19]
+    y3 = [6.58,5.76,7.71,8.84,8.47,7.04,5.25,5.56,7.91,6.89,12.50]
+    print("Dataset 3")
+    r = correlacao(x3, y3)
+    regressao(x3, y3, r)
 
 dataset1()
-
-"""
-x2 = [10,8,13,9,11,14,6,4,12,7,5]
-y2 = [9.14,8.14,8.47,8.77,9.26,8.10,6.13,3.10,9.13,7.26,4.74]
-print("Dados 2")
-correlacao(x2,y2)
-regressao(x2,y2)
-
-x3 = [8,8,8,8,8,8,8,8,8,8,19]
-y3 = [6.58,5.76,7.71,8.84,8.47,7.04,5.25,5.56,7.91,6.89,12.50]
-print("Dados 3")
-correlacao(x3,y3)
-regressao(x3,y3)
-"""
-
-
+dataset2()
+dataset3()
 
 
