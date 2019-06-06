57b1758fdb8e28ff32626308da90c26d855bfa3d
leticiawoelfer <leticia.woelfer@hotmail.com>
2019-06-05 22:05:51 -0300

    aula 05-06, fase 2

--- T4_CorrRegressao/rmdemo.py
+++ T4_CorrRegressao/rmdemo.py
@@ -3,22 +3,54 @@
 @authors: Douglas, Joana e Leticia
 """
 import matplotlib.pyplot as plt
-import math
 import csv
-
 
 def lerArquivo():
     with open('data.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             row.insert(0, 1)
-            X.append(row)
+            vetorPreco.append(row[3])
+            del row[3]
+            matrizX.append(row)
+
+def correlacao(x, y):
+    n = len(x)
+    somax = 0
+    for numX in x:
+        somax = somax + numX
+    mediaX = somax / n
+    
+    somay = 0
+    for numY in y:
+        somay = somay + numY
+    mediaY = somay / n
+    
+    somaR1 = 0
+    somaMediaX = 0
+    somaMediaY = 0
+    for i in range(0,n):
+        somaR1 = somaR1 + ((x[i] - mediaX) * (y[i] - mediaY))
+        somaMediaX = somaMediaX + ((x[i] - mediaX)**2)
+        somaMediaY = somaMediaY + ((y[i] - mediaY)**2)
         
+    raizQuad = math.sqrt(somaMediaX * somaMediaY)
+    
+    r = round((somaR1/raizQuad),4)
+    return r;
 
-X = []
+
+
+matrizX = []
+vetorPreco = []
+
 lerArquivo()
-print(X)
+#prepara a lista para fazer a correlacao e regressao linear
+tamCasa = []
+for i in range(0, (len(matrizX)-1)):
+    tamCasa.append(matrizX[i][1])
 
+correlacao(tamCasa, vetorPreco)
 
 
 
