4f1bb113bb4f41959da0ff476ae542d09313067c
leticiawoelfer <leticia.woelfer@hotmail.com>
2019-06-07 22:09:10 -0300

    Continuacao da fase 2

--- T4_CorrRegressao/rmdemo.py
+++ T4_CorrRegressao/rmdemo.py
@@ -3,15 +3,18 @@
 @authors: Douglas, Joana e Leticia
 """
 import matplotlib.pyplot as plt
+import math
 import csv
+import numpy as np
 
 def lerArquivo():
     with open('data.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
+        for row in csv_reader:            
+            vetorPreco.append(float(row[2]))
+            del row[2]
+            row = [int(x) for x in row]
             row.insert(0, 1)
-            vetorPreco.append(float(row[3]))
-            del row[3]
             matrizX.append(row)
 
 def correlacao(x, y):
@@ -39,23 +42,54 @@ def correlacao(x, y):
     r = round((somaR1/raizQuad),4)
     return r;
 
+def preparaLista(posicao, lista):
+    for i in range(0, (len(matrizX)-1)):
+        lista.append(int(matrizX[i][posicao]))
 
+def geraMatrizTransposta():
+    for i in range(0, 3):
+        linha = []
+        for j in range(0, len(matrizX)):
+            linha.append(matrizX[j][i])
+        matrizXTrans.append(linha)
+    print(matrizXTrans)
+    
+def calcularB():
+    a = np.dot(matrizX, matrizXTrans)
+    b = np.linalg.inv(a)
+    c = np.dot(vetorPreco,matrizXTrans)
+
+    print(b*c)
+    
+def calcularLinhaRegr():
+    pass
+#def regressao(x, y, r):
 
 matrizX = []
 vetorPreco = []
-
 lerArquivo()
+
 #prepara a lista para fazer a correlacao e regressao linear
 tamCasa = []
-for i in range(0, (len(matrizX)-1)):
-    tamCasa.append(int(matrizX[i][1]))
-
-correlacao(tamCasa, vetorPreco)
-
-
+preparaLista(1, tamCasa)    
+corr1 = correlacao(tamCasa, vetorPreco)
+print("Correlação Tamanho da casa e Preço: "+str(corr1))
+#regr = regressao
 
 
+#prepara a lista para fazer a correlacao e regressao linear
+numQuarts = []
+preparaLista(2, numQuarts)
+corr2 = correlacao(numQuarts, vetorPreco)
+print("Correlação Número de quartos e Preço: "+str(corr2))
 
+
+matrizXTrans = []
+geraMatrizTransposta()
+
+calcularB()
+
+calcularLinhaRegr()
 
 
 
