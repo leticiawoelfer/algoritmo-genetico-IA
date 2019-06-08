711c7e79c13c37fd283f0c25747dd2ccfb002aba
Douglas Portugal de Oliveira <douglas-portuga@hotmail.com>
2019-06-07 21:55:36 -0300

    Regreção no grafico

--- T4_CorrRegressao/demo_regressaop.py
+++ T4_CorrRegressao/demo_regressaop.py
@@ -22,16 +22,26 @@ def geraGrafico(matrizData):
     plt.figure()
 
 def polyfit(matrizData):
-    coefs = np.polynomial.polynomial.polyfit(matrizData[:,0], matrizData[:,1], 1)
-    ffit = np.poly1d(coefs)
-    x_new = np.linspace(matrizData[0,0], matrizData[-1,1], num=len(matrizData[:,0])*10)
+    coefs1 = np.polyfit(matrizData[:,0], matrizData[:,1], 1)
+    coefs1 = np.polyfit(matrizData[:,0], matrizData[:,1], 2)
+    coefs3 = np.polyfit(matrizData[:,0], matrizData[:,1], 3)
+    coefs8 = np.polyfit(matrizData[:,0], matrizData[:,1], 8)
+    
+    y1 = coefs1[1]+coefs1[0]*matrizData[:,0]
+    y2 = coefs3[2]+coefs3[1]*matrizData[:,0]+coefs3[0]*matrizData[:,0]**2
+    y3 = coefs3[3]+coefs3[2]*matrizData[:,0]+coefs3[1]*matrizData[:,0]**2+coefs3[0]*matrizData[:,0]**3
+    y8 = coefs8[8]+coefs8[7]*matrizData[:,0]+coefs8[6]*matrizData[:,0]**2+coefs8[5]*matrizData[:,0]**3+coefs8[4]*matrizData[:,0]**4+coefs8[3]*matrizData[:,0]**5+coefs8[2]*matrizData[:,0]**6+coefs8[1]*matrizData[:,0]**7+coefs8[0]*matrizData[:,0]**8
+    
     fig1 = plt.figure()                                                                                           
     ax1 = fig1.add_subplot(111)                                                                                   
     ax1.scatter(matrizData[:,0], matrizData[:,1])                                                                     
-    ax1.plot(x_new, ffit(x_new),'r')                                                                     
+    ax1.plot(matrizData[:,0], y1,'r')
+    ax1.plot(matrizData[:,0], y2,'g')
+    ax1.plot(matrizData[:,0], y3,'b')   
+    ax1.plot(matrizData[:,0], y8,'y')                                                              
     plt.show()
 
-geraGrafico(lerBaseDados())
+#geraGrafico(lerBaseDados())
 polyfit((lerBaseDados()))
 
 
